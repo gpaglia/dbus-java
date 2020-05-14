e6a55df2e5d42ced8f1b43b0c3f55ce80f03141d
Gian Enrico Paglia <gianenrico.paglia@gmail.com>
2020-05-14 18:30:40 +0200

    minor update to dbus-java-utils build script and some polishing of java code

--- dbus-java/src/test/java/sample/issue/ExportClass.java
+++ dbus-java/src/test/java/sample/issue/ExportClass.java
@@ -1,5 +1,4 @@
 package sample.issue;
-import java.io.IOException;
 
 import org.freedesktop.dbus.connections.impl.DBusConnection;
 import org.freedesktop.dbus.exceptions.DBusException;
@@ -7,31 +6,31 @@ import org.freedesktop.dbus.interfaces.DBusInterface;
 
 public class ExportClass implements DBusInterface {
 
-    @Override
-    public boolean isRemote() {
-        return false;
+  @Override
+  public boolean isRemote() {
+    return false;
+  }
+
+  @Override
+  public String getObjectPath() {
+    return "/";
+  }
+
+  public static void main(String[] args) throws DBusException, InterruptedException {
+    try (DBusConnection conn = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION)) {
+      conn.requestBusName("sample.issue");
+
+      ExportClass ex = new ExportClass();
+      conn.exportObject("/path", ex);
+
+      System.out.println("Exported object, waiting");
+      Thread.sleep(5000);
+
+      conn.unExportObject("/path");
+      System.out.println("Unexported object, waiting");
+
+      Thread.sleep(5000);
     }
-
-    @Override
-    public String getObjectPath() {
-        return "/";
-    }
-
-    public static void main(String[] args) throws DBusException, InterruptedException {
-        try (DBusConnection conn = DBusConnection.getConnection(DBusConnection.DBusBusType.SESSION)) {
-            conn.requestBusName( "sample.issue" );
-
-            ExportClass ex = new ExportClass();
-            conn.exportObject( "/path", ex);
-
-            System.out.println( "Exported object, waiting" );
-            Thread.sleep( 5000 );
-
-            conn.unExportObject( "/path" );
-            System.out.println( "Unexported object, waiting" );
-
-            Thread.sleep( 5000 );
-        }
-    }
+  }
 
 }
